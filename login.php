ec51cc50f29569cbacbd7388f9d267bdabad10a1
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2024-12-18 22:55:09 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,36 @@
+<?php
+// Oturum başlat
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Kullanıcı adını session değişkenine kaydet
+    $_SESSION['kullanici_adi'] = $_POST['kullanici_adi'];
+    // İkinci sayfaya yönlendir
+    header('Location: index.php');
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sayfa 1</title>
+    <link rel="stylesheet" href="loginStyle.css">
+</head>
+<body>
+<h1 style="color: aliceblue;">Welcome To Reality</h1><br>
+<form method="post" action="">
+    <input type="text" id="kullanici_adi" name="kullanici_adi" required hidden="true">
+
+    <div class="buttons">
+        <button class="glow-on-hover" onclick="document.getElementById('kullanici_adi').value = '1';" type="submit">Level1</button><br><br>
+        <button class="glow-on-hover" onclick="document.getElementById('kullanici_adi').value = '2';" type="submit">Level2</button><br><br>
+        <button class="glow-on-hover" onclick="document.getElementById('kullanici_adi').value = '3';" type="submit">Level3</button>
+    </div>
+</form>
+
+
+<script src="loginJS.js"></script>
+</body>
+</html>
